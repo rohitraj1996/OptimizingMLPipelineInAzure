6750d39079a6d2af9eeff1f00d2ab17bfa19bbbc
Rohit Raj <z.rohitraj1996@gmail.com>
2020-11-08 16:59:47 +0530

    Added code for Azure hyperdrive.

--- train.py
+++ train.py
@@ -14,13 +14,8 @@ from azureml.data.dataset_factory import TabularDatasetFactory
 # Data is located at:
 # "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
 
-ds = ### YOUR CODE HERE ###
-
-x, y = clean_data(ds)
-
-# TODO: Split data into train and test sets.
-
-### YOUR CODE HERE ###a
+file_url = "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
+ds = TabularDatasetFactory.from_delimited_files(path=file_url)
 
 run = Run.get_context()
 
@@ -49,7 +44,16 @@ def clean_data(data):
     x_df["poutcome"] = x_df.poutcome.apply(lambda s: 1 if s == "success" else 0)
 
     y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
+
+    return x_df, y_df
     
+
+x, y = clean_data(ds)
+
+# TODO: Split data into train and test sets.
+### YOUR CODE HERE ###
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=.20, random_state=42)
+
 
 def main():
     # Add arguments to script
