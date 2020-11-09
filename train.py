0fe5a6a9af084025a6786074dfc1653f1a8b5c52
Rohit Raj <z.rohitraj1996@gmail.com>
2020-11-09 14:45:01 +0530

    Added code for AutoML.

--- train.py
+++ train.py
@@ -71,8 +71,8 @@ def main():
 
     accuracy = model.score(x_test, y_test)
     run.log("Accuracy", np.float(accuracy))
-	
-	os.makedirs('outputs', exist_ok=True)
+    
+    os.makedirs('outputs', exist_ok=True)
     joblib.dump(model, 'outputs/model.joblib')
 
 if __name__ == '__main__':
